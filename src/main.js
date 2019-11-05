9e3bd311452dc55acc28c5f7feca22e71e630834
Andreas Eismann <andreas@eismaenners.de>
2019-11-05 12:49:23 +0100

    Split each expense directly

--- src/main.js
+++ src/main.js
@@ -1,35 +1,9 @@
 // jshint esversion: 6  
-
-
 import m from 'mithril';
 import tagl from 'tagl-mithril';
 
-
 // prettier-ignore
 const { address, aside, footer, header, h1, h2, h3, h4, h5, h6, hgroup, main, nav, section, article, blockquote, dd, dir, div, dl, dt, figcaption, figure, hr, li, ol, p, pre, ul, a, abbr, b, bdi, bdo, br, cite, code, data, dfn, em, i, kdm, mark, q, rb, rp, rt, rtc, ruby, s, samp, small, span, strong, sub, sup, time, tt, u, wbr, area, audio, img, map, track, video, embed, iframe, noembed, object, param, picture, source, canvas, noscript, script, del, ins, caption, col, colgroup, table, tbody, td, tfoot, th, thead, tr, button, datalist, fieldset, form, formfield, input, label, legend, meter, optgroup, option, output, progress, select, textarea, details, dialog, menu, menuitem, summary, content, element, slot, template } = tagl(m);
-
-const _data = JSON.parse(localStorage.getItem('data')) || {
-    users: [],
-};
-
-const sync = () => localStorage.setItem('data', JSON.stringify(_data));
-
-const addUser = name => {
-    _data.users.push({
-        name,
-        spendings: [],
-    });
-    sync();
-    return true;
-};
-
-const newUserInput = {
-    name: '',
-};
-
-const _vdata = {};
-
-const byName = userName => _data.users.find(user => user.name === userName);
 
 const range = N => {
     let r = [];
@@ -39,28 +13,79 @@ const range = N => {
     return r;
 };
 
-const maxSpent = () => _data.users.map(user => user.spendings.length).reduce((a,b)=>Math.max(a,b),0);
+const mapArray = (collection, fn = e=>e) => {
+    const result =  [];
+    for (let i = 0; i < collection.length; i++) {
+        result.push(fn(collection[i]));
+    }
+    return result;
+};
 
-const spending = (userName, idx) => byName(userName).spendings[idx] || 0.0;
-
-const sum = userName => byName(userName).spendings.reduce((a,b)=>a+b,0);
-
-const totalSum=() =>roundTwoDigits( _data.users.map(user=>sum(user.name)).reduce((a,b)=>a+b,0));
-
+const contains = (arr, e) => arr.indexOf(e) >= 0;
+const use = (v,fn) => fn(v);
 const roundTwoDigits = num => Math.round(num*100) / 100;
+const pluck = (arr,prop) => arr.map(e => e[prop]);
 
-const pay = userName => sum(userName) - totalSum() / (_data.users.length || 1);
+const _data = JSON.parse(localStorage.getItem('data')) || {
+    users: [],
+    expenses: [],
+};
+const users = () => _data.users;
+const expenses = () => _data.expenses;
+const sync = () => localStorage.setItem('data', JSON.stringify(_data));
 
-const use = (v,fn)=>fn(v);
+const newUserInput = {
+    name: '',
+};
+
+const expenseForUser = (expense,user) =>- ((expense.user === user? -expense.amount :0.0) + (contains(expense.users,user)? expense.amount/expense.users.length :0.0));
+const spending = (userName, expense) => expenseForUser(expense,userName) || 0.0;
+const sum = userName => expenses().map(expense=>expenseForUser(expense,userName)).reduce((a,b)=>a+b,0);
+const totalSum=() =>roundTwoDigits( users().map(user=>sum(user.name)).reduce((a,b)=>a+b,0));
+
+const addUser = name => {
+    users().push({
+        name
+    });
+    sync();
+    return true;
+};
+
+const addExpense = () => {
+    expenses().push({
+        title: nextExpense.title,
+        amount: nextExpense.amount,
+        user: nextExpense.user,
+        users: Object.keys(nextExpense.users).filter(u=>nextExpense.users[u])
+    });
+    Object.assign(nextExpense,{
+        title: '',
+        amount: 0.0,
+        user: '',
+        users: {}
+    });
+    sync();
+};
+
+const nextExpense = {
+    title: '',
+    amount: 0.0,
+    user: '',
+    users: {}
+};
+
+const currencies = [
+    '$','€','¥','£'
+];
 
 m.mount(document.body, {
     view: vnode => [
-        h1('Splitter' ,              button.small({onclick:e=>{
-            _data.users=[];
-        }},'clear'),
-        small(
-            totalSum()
-        )),
+        h1('Splitter' ,              
+            button.small({onclick:e=> (_data.users = [], _data.expenses = [])},'clear'),
+            select({oninput: e=>(_data.currency=e.target.value, sync())},
+            currencies.map(c=>option(c))
+        ),small(totalSum())),
+        
         hr(),
         input({
             value: newUserInput.name,
@@ -68,52 +93,41 @@ m.mount(document.body, {
         }),
         button({onclick: e => addUser(newUserInput.name) && (newUserInput.name = '')}, 'Add User'),
         table.fullHeight(
-            thead(tr(_data.users.map(user => th(user.name)))),
+            thead(tr(th(),users().map(user => th(user.name)))),
             tbody(
-                range(maxSpent()).map(
-                    idx=>tr(
-                        _data.users
-                        .map(user=>user.name)
-                        .map(userName=>td( roundTwoDigits( spending(userName,idx))))
+                expenses().map(
+                    expense => tr({'data-label':expense.title},
+                    td(expense.title,'(',expense.amount,_data.currency,')'),   
+                    users()
+                        .map(user => user.name)
+                        .map(userName => td({'data-label':userName}, roundTwoDigits( spending(userName,expense))))
                     )
                 ),
-                tr(),
-                tr(
-                    _data.users.map(user=>roundTwoDigits(sum(user.name))).map(sum=>td.total(sum))
-                ),
-                tr(
-                    _data.users.map(user=>use(roundTwoDigits(pay(user.name)), pay => 
-                    td[{true:'positive',false:'negative'}[pay > 0]](pay)))
-                
-                ),
-                tr(
-                    _data.users.map(user =>
-                        td(
-                            input({
-                                type: 'number',
-                                value: _vdata[user.name],
-                                oninput: e => (_vdata[user.name] = parseFloat(e.target.value)),
-                            }),
-                            button(
-                                {
-                                    onclick: e => {
-                                        if (_vdata[user.name] !== 0.0) {
-                                            byName(user.name).spendings.push(_vdata[user.name]);
-                                            _vdata[user.name] = 0.0;
-                                            sync();
-                                        }
-                                    },
-                                },
-                                '+'
-                            )
-                        )
-                    )
+                tr(td(b('Sum')),
+                    users().map(user => use( roundTwoDigits(sum(user.name)),sum=>td[{true:'positive',false:'negative'}[sum > 0]](sum,_data.currency)))
                 )
+//                tr(td({colspan: users().length}, ))
             )
         ),
-        textarea({value: JSON.stringify(_data.users,null,2),oninput: e => {
+        [
+            input({placeHolder:'Expense', value: nextExpense.title, oninput: e=>nextExpense.title=e.target.value}),
+            input({type:'number', value: nextExpense.amount, oninput: e=>nextExpense.amount=parseFloat(e.target.value)}),
+            select({value: nextExpense.user,oninput:e => nextExpense.user = e.target.value},
+                users().map(user=>option(user.name))
+            ),
+            div.buttonGroup(
+                users().map(user=>button[{true:'inverse', false:''}[!!nextExpense.users[user.name]]]({onclick:e=>nextExpense.users[user.name]=!nextExpense.users[user.name]},user.name))
+            ),
+            button.tertiary({
+                onclick:e=>{
+                    addExpense();
+                }
+            }, '+'),
+            br()
+        ],
+        textarea({value: JSON.stringify(_data, null, 2), oninput: e => {
           let dat = JSON.parse(e.target.value);
-          _data.users = dat;
+         Object.assign(_data,dat);
         } })
     ]
     
