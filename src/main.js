4fafa2df468dec8a0b19e20f9e47cef672567fe1
Andreas Eismann <andreas@eismaenners.de>
2019-11-06 01:27:11 +0100

    New distro

--- src/main.js
+++ src/main.js
@@ -7,14 +7,14 @@ const { address, aside, footer, header, h1, h2, h3, h4, h5, h6, hgroup, main, na
 
 const range = N => {
     let r = [];
-    for(let i = 0; i < N; i++) {
+    for (let i = 0; i < N; i++) {
         r.push(i);
     }
     return r;
 };
 
-const mapArray = (collection, fn = e=>e) => {
-    const result =  [];
+const mapArray = (collection, fn = e => e) => {
+    const result = [];
     for (let i = 0; i < collection.length; i++) {
         result.push(fn(collection[i]));
     }
@@ -28,20 +28,20 @@ function b64EncodeUnicode(str) {
     return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g,
         function toSolidBytes(match, p1) {
             return String.fromCharCode('0x' + p1);
-    }));
+        }));
 }
 
 function b64DecodeUnicode(str) {
     // Going backwards: from bytestream, to percent-encoding, to original string.
-    return decodeURIComponent(atob(str).split('').map(function(c) {
+    return decodeURIComponent(atob(str).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
 }
 
 const contains = (arr, e) => arr.indexOf(e) >= 0;
-const use = (v,fn) => fn(v);
-const roundTwoDigits = num => Math.round(num*100) / 100;
-const pluck = (arr,prop) => arr.map(e => e[prop]);
+const use = (v, fn) => fn(v);
+const roundTwoDigits = num => Math.round(num * 100) / 100;
+const pluck = (arr, prop) => arr.map(e => e[prop]);
 
 const _data = JSON.parse(localStorage.getItem('data')) || {
     users: [],
@@ -51,23 +51,23 @@ const _data = JSON.parse(localStorage.getItem('data')) || {
 
 console.log(window.location.search)
 const updateURL = () => {
-    window.location.search = m.buildQueryString({data: b64EncodeUnicode(JSON.stringify(_data))})
+    window.location.search = m.buildQueryString({ data: b64EncodeUnicode(JSON.stringify(_data)) })
     return true;
 };
 
- 
+
 const users = () => _data.users;
 const expenses = () => _data.expenses;
-const sync = () =>updateURL()&& localStorage.setItem('data', JSON.stringify(_data));
+const sync = () => updateURL() && localStorage.setItem('data', JSON.stringify(_data));
 
 const newUserInput = {
     name: '',
 };
 
-const expenseForUser = (expense,user) =>- ((expense.user === user? -expense.amount :0.0) + (contains(expense.users,user)? expense.amount/expense.users.length :0.0));
-const spending = (userName, expense) => expenseForUser(expense,userName) || 0.0;
-const sum = userName => expenses().map(expense=>expenseForUser(expense,userName)).reduce((a,b)=>a+b,0);
-const totalSum=() =>roundTwoDigits( users().map(user=>sum(user.name)).reduce((a,b)=>a+b,0));
+const expenseForUser = (expense, user) => - ((expense.user === user ? -expense.amount : 0.0) + (contains(expense.users, user) ? expense.amount / expense.users.length : 0.0));
+const spending = (userName, expense) => expenseForUser(expense, userName) || 0.0;
+const sum = userName => expenses().map(expense => expenseForUser(expense, userName)).reduce((a, b) => a + b, 0);
+const totalSum = () => roundTwoDigits(users().map(user => sum(user.name)).reduce((a, b) => a + b, 0));
 
 const addUser = name => {
     users().push({
@@ -82,9 +82,9 @@ const addExpense = () => {
         title: nextExpense.title,
         amount: nextExpense.amount,
         user: nextExpense.user,
-        users: Object.keys(nextExpense.users).filter(u=>nextExpense.users[u])
+        users: Object.keys(nextExpense.users).filter(u => nextExpense.users[u])
     });
-    Object.assign(nextExpense,{
+    Object.assign(nextExpense, {
         title: '',
         amount: 0.0,
         user: '',
@@ -111,7 +111,7 @@ const nextExpense = {
 };
 
 const currencies = [
-    '$','€','¥','£'
+    '$', '€', '¥', '£'
 ];
 
 let show = true;
@@ -120,74 +120,69 @@ const showExpenser = () => show = !show;
 
 m.mount(document.body, {
     view: vnode => [
-
         [
             div.row(
-            m("div", {"class":"col-sm-8"},
-            
-            h1('Splitter' ,              
-            button.small({onclick:e=> (_data.users = [], _data.expenses = [])},'clear'),
-            select({oninput: e=>(_data.currency=e.target.value, sync())},
-            currencies.map(c=>option(c))
-        ),small(totalSum())),
-
-            ), 
-            m("div", {"class":"col-sm-4"},
-            input({
-                value: newUserInput.name,
-                oninput: e => (newUserInput.name = e.target.value),
-            }),
-            button({onclick: e => addUser(newUserInput.name) && (newUserInput.name = '')}, 'Add User')
-            ))
-          ],
-
-        
-        hr(),
-        
-        show?  div.w3AnimateRight([table.fullHeight(
-            thead(tr(th(),users().map(user => th(user.name)))),
-            tbody(
-                expenses().map(
-                    expense => tr({'data-label':expense.title},
-                    td(expense.title,'(',expense.amount,_data.currency,')'),   
-                    users()
-                        .map(user => user.name)
-                        .map(userName => td({'data-label':userName}, roundTwoDigits( spending(userName,expense))))
-                    )
+                m("div", { "class": "col-sm-8" },
+                    h1('Splitter',
+                        button.small({ onclick: e => (_data.users = [], _data.expenses = []) }, 'clear'),
+                        select({ value:_data.currency, oninput: e => (_data.currency = e.target.value, sync()) },
+                            currencies.map(c => option(c))
+                        ), small(totalSum())),
                 ),
-                tr(td(b('Sum')),
-                    users().map(user => use( roundTwoDigits(sum(user.name)),sum=>td[{true:'positive',false:'negative'}[sum > 0]](sum,_data.currency)))
+                m("div", { "class": "col-sm-4" },
+                    input({
+                        value: newUserInput.name,
+                        oninput: e => (newUserInput.name = e.target.value),
+                    }),
+                    button({ onclick: e => addUser(newUserInput.name) && (newUserInput.name = '') }, 'Add User')
+                ))
+        ],
+        hr(),
+        show ? div.w3AnimateRight([
+            table.fullHeight(
+                thead(tr(th(), users().map(user => th(user.name)))),
+                tbody(
+                    expenses().map(
+                        expense => tr({ 'data-label': expense.title },
+                            td(expense.title, '(', expense.amount, _data.currency, ')'),
+                            users()
+                                .map(user => user.name)
+                                .map(userName => td({ 'data-label': userName }, roundTwoDigits(spending(userName, expense))))
+                        )
+                    ),
+                    tr(td(b('Sum')),
+                        users().map(user => use(roundTwoDigits(sum(user.name)), sum => td[{ true: 'positive', false: 'negative' }[sum > 0]](sum, _data.currency)))
+                    )
+                    //                tr(td({colspan: users().length}, ))
                 )
-//                tr(td({colspan: users().length}, ))
-            )
-        ),
-        button.primary({
-            onclick:e=>{
-                showExpenser();
-            }
-        }, '+')])
-        :div.w3AnimateLeft([
-            label('What?'),
-            input({placeHolder:'Expense', value: nextExpense.title, oninput: e=>nextExpense.title=e.target.value}),
-            label('How much?'),
-            input({type:'number', value: nextExpense.amount, oninput: e=>nextExpense.amount=parseFloat(e.target.value)}),
-            label('Who payed it?'),
-            select({value: nextExpense.user,oninput:e => nextExpense.user = e.target.value},
-                users().map(user=>option(user.name))
             ),
-            label('Who enjoyed it?'),
-            div.buttonGroup(
-                users().map(user=>button[{true:'inverse', false:''}[!!nextExpense.users[user.name]]]({onclick:e=>nextExpense.users[user.name]=!nextExpense.users[user.name]},user.name))
-            ),
-            button.tertiary({
-                onclick:e=>{
-                    addExpense();
+            button.primary({
+                onclick: e => {
+                    showExpenser();
                 }
-            }, '+'),
+            }, '+')])
+            : div.w3AnimateLeft([
+                label('What?'),
+                input({ placeHolder: 'Expense', value: nextExpense.title, oninput: e => nextExpense.title = e.target.value }),
+                label('How much?'),
+                input({ type: 'number', value: nextExpense.amount, oninput: e => nextExpense.amount = parseFloat(e.target.value) }),
+                label('Who payed it?'),
+                select({ value: nextExpense.user, oninput: e => nextExpense.user = e.target.value },
+                    users().map(user => option(user.name))
+                ),
+                label('Who enjoyed it?'),
+                div.buttonGroup(
+                    users().map(user => button[{ true: 'inverse', false: '' }[!!nextExpense.users[user.name]]]({ onclick: e => nextExpense.users[user.name] = !nextExpense.users[user.name] }, user.name))
+                ),
+                button.tertiary({
+                    onclick: e => {
+                        addExpense();
+                    }
+                }, '+'),
 
-        ]),
+            ]),
         [
-/*,*/            ,
+/*,*/,
 
             br()
         ],
@@ -195,8 +190,8 @@ m.mount(document.body, {
           let dat = JSON.parse(e.target.value);
          Object.assign(_data,dat);
         } }),*/
-        [ 
+        [
         ]
     ]
-    
+
 });
